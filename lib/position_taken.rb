67bd39b88a29039c41bd869d7a712ec3d8d66f14
Olivia Cubela <oliviacubela@gmail.com>
2020-05-11 19:08:17 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,34 @@
-# code your #position_taken? method here!
+def position_taken? (board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0,8)
+    return true
+  else
+    return false
+  end
+end
+
+#def position_taken?(index, board)
+#  index = 0
+#  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+#  " " == ""
+#end
+
+#def current_position(board)
+#if position_taken?(board) == " " || ""
+#  return "false"
+#  elsif position_taken?(board) == " " || ""
+#    return "true"
+#  end
+#end
+
+
+# def position_taken?(board, index)
+#  !(board[index].nil? || board[index] == " ")
+# end
