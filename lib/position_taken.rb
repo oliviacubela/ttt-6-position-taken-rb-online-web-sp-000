38395fa278dd28ca8aa332e3bab23416d64ff12c
Olivia Cubela <oliviacubela@gmail.com>
2020-07-08 17:24:23 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -6,13 +6,6 @@ def position_taken? (board, index)
   end
 end
 
-def valid_move?(board, index)
-  if !position_taken?(board, index) && (index).between?(0,8)
-    return true
-  else
-    return false
-  end
-end
 
 #def position_taken?(index, board)
 #  index = 0
